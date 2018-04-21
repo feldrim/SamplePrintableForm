ed816ee7222857600d780817ad09e22fbb160436
the f <feldrim@gmail.com>
2018-04-21 17:29:41 +0300

    Add project files.

---
+++ SamplePrintableForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SamplePrintableForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
